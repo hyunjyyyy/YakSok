172e3e2310260626406a674f72259dbbe5c5ec76
yerin <monicayerin40@gmail.com>
2025-09-26 18:03:56 +0900

    디렉터리 구조 생성

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default GlobalStyle;
