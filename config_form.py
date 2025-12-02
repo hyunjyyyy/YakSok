7a689cb9aa5914311998d082b9179089b91f6b7a
hyunjyyyy <yuuhyunji@sookmyung.ac.kr>
2025-12-02 19:06:58 +0900

    chmod: upload config_form.py

---
+++ config_form.py
@@ -0,0 +1,12 @@
+# config.py
+
+# 개발 환경 데이터베이스 설정
+# 실제 운영 환경에서는 환경 변수를 사용하거나, 별도의 보안 파일을 통해 관리하는 것을 권장합니다.
+DB_CONFIG = {
+    'user': 'root',
+    'host': 'localhost',
+    'password':'realpassword',
+    'database': 'yaksok',
+    'port':'3306'
+}
+GEMINI_API_KEY='real_gemini_api_key'
